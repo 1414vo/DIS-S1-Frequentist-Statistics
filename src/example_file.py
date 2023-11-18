711878ba4f4b8a66445564b3b13afedf6b1f642f
Ivo Petrov <1414vo@gmail.com>
2023-11-18 14:21:17 +0000

    Setup Doxygen

---
+++ src/example_file.py
@@ -0,0 +1,8 @@
+"""!@file example_file.py
+@brief Contains an example of how docstrings should be formatted for future reference.
+    Will be removed upon starting the project.
+
+@details Contains an example of how docstrings should be formatted for future reference.
+    Will be removed upon starting the project.
+@author Created by I. Petrov on 18/11/2023
+"""
