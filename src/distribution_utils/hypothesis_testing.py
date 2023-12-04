fc4443cb89bd8dfc7dc991dfa28bc1312abe66f3
Ivo Petrov <1414vo@gmail.com>
2023-12-04 01:06:44 +0000

    Binary search for threshold finding, optimized functions

--- src/distribution_utils/hypothesis_testing.py
+++ src/distribution_utils/hypothesis_testing.py
@@ -1,8 +1,10 @@
-from src.distribution_utils.distribution import partial_pdf, generate_sample
+from src.distribution_utils.distributions import partial_pdf, generate_sample
 from src.distribution_utils.estimation import (
     unbinned_mle_estimation,
     binned_mle_estimation,
 )
+from scipy.stats import chi2
+from functools import partial
 import numpy as np
 
 
@@ -46,9 +48,7 @@ def hypothesis_test(sample, h0, h1, statistic=log_likelihood_ratio, **kwargs):
     return statistic(sample, h0_fit, h1_fit)
 
 
-def binned_hypothesis_test(
-    sample, h0_pdf, h0_cdf, h1_pdf, h1_cdf, statistic=log_likelihood_ratio, **kwargs
-):
+def binned_hypothesis_test(sample, h0, h1, statistic=log_likelihood_ratio, **kwargs):
     if "null_params" in kwargs:
         null_params = kwargs["null_params"]
     else:
@@ -75,14 +75,15 @@ def binned_hypothesis_test(
     alternate_limits["beta"] = (max(sample) + 1e-5, None)
 
     null_fit_params, _, _ = binned_mle_estimation(
-        sample, h0_cdf, params=null_params, limits=null_limits
-    )
-    alternate_fit_params, _, _ = binned_mle_estimation(
-        sample, h1_cdf, params=alternate_params, limits=alternate_limits
+        sample, h0["cdf"], params=null_params, limits=null_limits
     )
 
-    h0_fit = partial_pdf(pdf=h0_pdf, **null_fit_params.to_dict())
-    h1_fit = partial_pdf(pdf=h1_pdf, **alternate_fit_params.to_dict())
+    alternate_fit_params, _, _ = binned_mle_estimation(
+        sample, h1["cdf"], params=alternate_params, limits=alternate_limits
+    )
+
+    h0_fit = partial_pdf(pdf=h0["pdf"], **null_fit_params.to_dict())
+    h1_fit = partial_pdf(pdf=h1["pdf"], **alternate_fit_params.to_dict())
 
     return statistic(sample, h0_fit, h1_fit)
 
@@ -112,15 +113,15 @@ def test_statistic_distribution(
 
 
 def binned_test_statistic_distribution(
-    h0, h1, h0_cdf, h1_cdf, bounds, stat, n_samples=10000, sample_size=1000, **kwargs
+    h0, h1, stat, n_samples=10000, sample_size=1000, **kwargs
 ):
     null_stat = []
     alt_stat = []
-    h0_fit = partial_pdf(h0, **kwargs["null_fit"])
-    h1_fit = partial_pdf(h1, **kwargs["alt_fit"])
     for _ in range(n_samples):
-        null_sample = generate_sample(h0_fit, *bounds, N=sample_size, random_seed=None)
-        alt_sample = generate_sample(h1_fit, *bounds, N=sample_size, random_seed=None)
+        null_sample = h0["rvs"](
+            **kwargs["null_fit"], size=sample_size, random_state=None
+        )
+        alt_sample = h1["rvs"](**kwargs["alt_fit"], size=sample_size, random_state=None)
 
         args = {
             "null_params": kwargs["null_fit"],
@@ -128,21 +129,65 @@ def binned_test_statistic_distribution(
             "alternate_params": kwargs["alt_fit"],
             "alternate_limits": kwargs["alt_limits"],
         }
-
-        null_stat.append(
-            binned_hypothesis_test(null_sample, h0, h0_cdf, h1, h1_cdf, stat, **args)
-        )
-        alt_stat.append(
-            binned_hypothesis_test(alt_sample, h0, h0_cdf, h1, h1_cdf, stat, **args)
-        )
+        null_args = args
+        null_args["alternate_params"] = kwargs["alt_init"]
+        null_stat.append(binned_hypothesis_test(null_sample, h0, h1, stat, **args))
+        alt_stat.append(binned_hypothesis_test(alt_sample, h0, h1, stat, **args))
 
     return np.array(null_stat), np.array(alt_stat)
 
 
-def get_statistic_p_values(dof, alternate_distribution):
-    pass
+def get_t2_error(h0_distribution, h1_distribution, t1_error_rate):
+    def chi2_pdf(X, df):
+        return chi2.pdf(X, df=df)
+
+    chi2_fit, _, _ = unbinned_mle_estimation(
+        h0_distribution, chi2_pdf, params={"df": 3}, limits={"df": (0, None)}
+    )
+    t_threshold = chi2.ppf(1 - t1_error_rate, df=chi2_fit["df"])
+    print(chi2_fit)
+    return np.count_nonzero(h1_distribution < t_threshold) / len(h1_distribution)
 
 
-def find_size_threshold(h0, h1, t1_error_rate=2.9e-7, t2_error_rate=0.1, n_0=500):
+def find_size_threshold(
+    h0, h1, t1_error_rate=2.9e-7, t2_error_rate=0.1, n_samples=10000, n_0=500, **kwargs
+):
+    val_range = (0, n_0)
+    low_score = 1
+    get_statistic = partial(
+        binned_test_statistic_distribution,
+        h0=h0,
+        h1=h1,
+        stat=log_likelihood_ratio,
+        null_fit=kwargs["null_fit"],
+        null_limits=kwargs["null_limits"],
+        alt_fit=kwargs["alt_fit"],
+        alt_limits=kwargs["alt_limits"],
+        alt_init=kwargs["alt_init"],
+        n_samples=n_samples,
+    )
+    high_score = get_t2_error(*get_statistic(sample_size=n_0), t1_error_rate)
+    print(val_range, low_score, high_score)
     # Find upper limit of range
-    pass
+    while high_score > t2_error_rate:
+        low_score, high_score = high_score, get_t2_error(
+            *get_statistic(sample_size=2 * val_range[1]), t1_error_rate
+        )
+        val_range = (val_range[1], val_range[1] * 2)
+        print(val_range, low_score, high_score)
+
+    # Binary search
+    while val_range[1] - val_range[0] != 1:
+        print(val_range, high_score)
+        sample_value = (val_range[0] + val_range[1]) // 2
+        new_score = get_t2_error(
+            *get_statistic(sample_size=sample_value), t1_error_rate
+        )
+        if new_score < t2_error_rate:
+            val_range = (val_range[0], sample_value)
+            high_score = new_score
+        else:
+            val_range = (sample_value, val_range[1])
+            low_score = new_score
+        print(val_range, low_score, high_score)
+    return val_range[1]
--- src/distribution_utils/optimized_distributions.py
+++ src/distribution_utils/optimized_distributions.py
@@ -13,7 +13,8 @@ r"""! @file distributions.py
 @author Created by I. Petrov on 02/12/2023
 """
 
-from numba_stats import truncnorm, truncexpon
+from numba_stats import truncnorm, truncexpon, uniform
+import numpy as np
 
 
 def background_pdf_optimized(M, lam: float, alpha: float, beta: float):
@@ -21,8 +22,6 @@ def background_pdf_optimized(M, lam: float, alpha: float, beta: float):
     \f$p(M; \lambda) = N \lambda e^{-\lambda M}\f$ in the range \f$[\alpha, \beta]\f$.
 
     @param M                A value or iterable of observations.
-    @param f                The fraction of the distribution attributed to the normal component.
-                            Must be a real number between 0 and 1.
     @param lam              The \f$\lambda\f$ parameter of the exponential distribution.
                             Must be a positive number.
     @param alpha            The lower bound of the distribution. Must be non-negative.
@@ -31,7 +30,7 @@ def background_pdf_optimized(M, lam: float, alpha: float, beta: float):
     @return                 The probability density functions of the observations.
     """
 
-    return truncexpon.pdf(M, loc=0, scale=1 / lam, xmin=alpha, xmax=beta)
+    return truncexpon.pdf(M, loc=0.0, scale=1 / lam, xmin=alpha, xmax=beta)
 
 
 def distribution_pdf_optimized(
@@ -56,31 +55,29 @@ def distribution_pdf_optimized(
 
     return f * truncnorm.pdf(M, loc=mu, scale=sigma, xmin=alpha, xmax=beta) + (
         1 - f
-    ) * truncexpon.pdf(M, loc=0, scale=1 / lam, xmin=alpha, xmax=beta)
+    ) * truncexpon.pdf(M, loc=0.0, scale=1 / lam, xmin=alpha, xmax=beta)
 
 
 def background_cdf_optimized(M, lam: float, alpha: float, beta: float):
-    r"""! Computes the probability density function for the distribution
+    r"""! Computes the cumulative density function for the distribution
     \f$p(M; \lambda) = N \lambda e^{-\lambda M}\f$ in the range \f$[\alpha, \beta]\f$.
 
     @param M                A value or iterable of observations.
-    @param f                The fraction of the distribution attributed to the normal component.
-                            Must be a real number between 0 and 1.
     @param lam              The \f$\lambda\f$ parameter of the exponential distribution.
                             Must be a positive number.
     @param alpha            The lower bound of the distribution. Must be non-negative.
     @param beta             The upper bound of the distribution. Must be larger than alpha.\
 
-    @return                 The probability density functions of the observations.
+    @return                 The cumulative density functions of the observations.
     """
 
-    return truncexpon.cdf(M, loc=0, scale=1 / lam, xmin=alpha, xmax=beta)
+    return truncexpon.cdf(M, loc=alpha, scale=1 / lam, xmin=alpha, xmax=beta)
 
 
 def distribution_cdf_optimized(
     M, f: float, lam: float, mu: float, sigma: float, alpha: float, beta: float
 ):
-    r"""! Computes the normalization for the distribution
+    r"""! Computes the cumulative density function for the distribution
     \f$p(M; f,\lambda,\mu,\sigma) = fs(M;\mu, \sigma) + (1-f)b(M; \lambda)\f$.
 
     @param M        A value or iterable of observations
@@ -93,10 +90,75 @@ def distribution_cdf_optimized(
     @param alpha    The lower bound of the distribution. Must be non-negative.
     @param beta     The upper bound of the distribution. Must be larger than alpha.
 
-    @return         The value of the pdf at a given point
+    @return         The value of the cdf at a given point
                     (or multiple points if input is iterable).
     """
-
     return f * truncnorm.cdf(M, loc=mu, scale=sigma, xmin=alpha, xmax=beta) + (
         1 - f
-    ) * truncexpon.cdf(M, loc=0, scale=1 / lam, xmin=alpha, xmax=beta)
+    ) * truncexpon.cdf(M, loc=alpha, scale=1 / lam, xmin=alpha, xmax=beta)
+
+
+def generate_background_samples(
+    lam: float, alpha: float, beta: float, size: int, random_state: int = None
+):
+    r"""! Generates a sample for the distribution
+    \f$p(M; \lambda) = N \lambda e^{-\lambda M}\f$ in the range \f$[\alpha, \beta]\f$.
+
+    @param lam      The \f$\lambda\f$ parameter of the exponential distribution.
+                    Must be a positive number.
+    @param alpha    The lower bound of the distribution. Must be non-negative.
+    @param beta     The upper bound of the distribution. Must be larger than alpha.
+    @param size     The length of the sample.
+
+    @return         A sample of size given by the size variable.
+    """
+    return truncexpon.rvs(
+        xmin=alpha,
+        xmax=beta,
+        loc=0.0,
+        scale=1 / lam,
+        size=size,
+        random_state=random_state,
+    )
+
+
+def generate_distribution_samples(
+    f: float,
+    lam: float,
+    mu: float,
+    sigma: float,
+    alpha: float,
+    beta: float,
+    size: int,
+    random_state: int = None,
+):
+    r"""! Generates a sample for the distribution
+    \f$p(M; f,\lambda,\mu,\sigma) = fs(M;\mu, \sigma) + (1-f)b(M; \lambda)\f$.
+
+    @param M            A value or iterable of observations
+    @param f            The fraction of the distribution attributed to the normal component.
+                        Must be a real number between 0 and 1.
+    @param lam          The \f$\lambda\f$ parameter of the exponential distribution.
+                        Must be a positive number.
+    @param mu           The mean of the normal distribution.
+    @param sigma        The standard deviation of the normal distribution. Must be positive.
+    @param alpha        The lower bound of the distribution. Must be non-negative.
+    @param beta         The upper bound of the distribution. Must be larger than alpha.
+    @param size         The length of the sample.
+    @param random_state The random state seed.
+
+    @return         A sample of size given by the size variable.
+    """
+    classes = uniform.rvs(a=0, w=1, size=size, random_state=random_state)
+    exp = truncexpon.rvs(
+        xmin=alpha,
+        xmax=beta,
+        loc=alpha,
+        scale=1 / lam,
+        size=size,
+        random_state=random_state,
+    )
+    norm = truncnorm.rvs(
+        loc=mu, scale=sigma, xmin=alpha, xmax=beta, size=size, random_state=random_state
+    )
+    return np.where(classes < f, norm, exp)
